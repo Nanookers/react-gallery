ae00e008a0ce71cf9965fcd1e69637ca32588a66
Connor Clark <connordeclark@gmail.com>
2023-01-14 17:59:57 -0600

    Rough fin base

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,6 +2,9 @@ const galleryItems = [
     { id: 1, path: './images/squidCone.jpg', description: 'The ultimate punishment; the sheer degradation of it; the final nail in his metaphorical coffin .', likes: 0 },
     { id: 2, path: './images/squidInTHought.jpg', description: 'Photo of a Guinea Pig in the Wild.', likes: 0 },
     { id: 3, path: './images/squidAngry.jpg', description: 'Photo of a Guinea Pig in the Wild.', likes: 0 },
+    { id: 4, path: './images/squidBirthday.jpg', description: 'Photo of a Guinea Pig in the Wild.', likes: 0 },
+    { id: 5, path: './images/squidBadHaircut.jpg', description: 'Photo of a Guinea Pig in the Wild.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
